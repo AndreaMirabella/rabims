9643c6ad64903959989b20bfde468394c85dc77c
Marco De Bortoli <marco.pk.debortoli@gmail.com>
2010-11-28 01:48:07 +0000

    Added initial draft project structure

---
+++ application/locale/it_IT/main.php
@@ -0,0 +1,114 @@
+<?php
+	$translate = array (
+		
+		// INDEX
+		Title		 						=> 'Servizi Rabi Multiservices',
+		Home		 						=> 'Pagina Iniziale',
+		
+		// MENU
+		MenuShowTickets						=> 'Visualizza Tickets',
+		MenuPasswordRetreive				=> 'Recupero dati',
+		MenuTicketReply						=> 'Rispondi',
+		
+		// TICKET PAGE
+		ShowTickets							=> 'Visualizza Tickets',
+		Informations						=> 'Informazioni',
+		TicketNumber						=> 'Numero Ticket',
+		Status								=> 'Stato',
+		StatusValueOpen						=> 'Aperto',
+		StatusValueClose					=> 'Chiuso',
+		TicketCreation						=> 'Creato il',
+		TicketClose							=> 'Chiuso il',
+		Actions								=> 'Azioni',
+		ReOpen								=> 'Riapri il ticket',
+		ReClose								=> 'Chiudi il ticket',
+		PostReply							=> 'Rispondi',
+		PagePrint							=> 'Stampa questa pagina',
+		Favourite							=> 'Aggiungi ai preferiti',
+		TicketTitle							=> 'Ticket',
+		Subject								=> 'Oggetto',
+		NoMessages							=> 'Nessuna domanda presente',
+		
+		PanelTitle							=> 'Pannello di controllo',
+		Profile								=> 'Profilo',
+		Logout								=> 'Disconnetti',
+		
+		// TICKET ACTION: REPLY
+		ReplyTicketTitle					=> 'Risposta/Domanda',
+		ReplyTicketAttacchedTitle			=> 'Allegati',
+		ReplyTicketAttacchedSingleTitle		=> 'Allega',
+		ReplyTicketAttacchedMaxSize			=> 'Dimensione max allegati 5 MB',
+		
+		// INDEX 
+		IndexTitle							=> 'Home page',
+		LoginPanel							=> 'Autenticazione',
+		LoginUsername						=> 'Utente',
+		LoginPassword						=> 'Password',
+		PasswordRetreive					=> 'Recupero dati',
+		InfoTitle							=> 'Info &amp; Recapiti',
+		ServicesTitle						=> 'Servizi disponibili',
+		NoServices							=> 'Nessun Servizio disponibile',
+		IndexServices						=> 'Ecco i servizi messi a disposizione da Rabi Multiservices',
+		
+		// FORGOT 
+		PasswordRetreiveDescription			=> 'In questa sezione &egrave; possibile recuperare le credenziali di accesso in pochi semplici passi',
+		PasswordRetreiveEmail				=> 'Indirizzo email',
+		PasswordRetreiveMobile				=> 'Numero di cellulare',
+		PasswordRetreiveOption1				=> 'Tramite indirizzo email',
+		PasswordRetreiveOption2				=> 'Tramite numero di cellulare',
+		PasswordRetreiveStep1				=> 'Inserisci l\'indirizzo email fornito in fase di registrazione',
+		PasswordRetreiveStep2				=> 'Clicca sul link che ti verr&agrave; inviato via email',
+		PasswordRetreiveStep3				=> 'Inserisci una nuova password',
+		PasswordRetreiveStep1a				=> 'Inserisci il numero di cellulare fornito in fase di registrazione',
+		PasswordRetreiveStep2a				=> 'Inserisci il codice che ti &egrave; stato inviato',
+		PasswordRetreiveStep3a				=> 'Inserisci una nuova password',
+		PasswordRetreiveButton				=> 'Recupera i dati di accesso',
+		
+		// MENU
+		Service1							=> 'Guide',
+		Service1Desc						=> 'Sezione contentente informazioni tecniche, manuali e guide utili all\' utente costantemente aggiornate che riprendono le problematiche pi� frequenti.',
+		Service2							=> 'Tickets',
+		Service2Desc						=> 'In questa sezione &egrave; possibile inviare nuove richieste di tipo tecnico/amministrativo o visualizzare/aggiornare ticket gi&agrave; aperti.',
+		Service3							=> 'Assistenza',
+		Service3Desc						=> '',
+		Service4							=> 'E-learning',
+		Service4Desc						=> '',
+		
+		// BOTTONI 
+		RegisterButton 						=> 'Registrati',
+		ResetButton 						=> 'Azzera campi',
+		SendButton 							=> 'Invia',
+		
+		// MESSAGGI GENERICI
+		JavascriptsDisabled 				=> 'Javascripts disabilitati',
+		JavascriptDisabledMessages 			=> 'Per procedere &egrave; necessario abilitare i Javascripts',
+		CompilationErrors 					=> 'ATTENZIONE!!! Errori in fase di compilazione',
+		CompilationEmailMismatching 		=> 'ATTENZIONE!!! Gli indirizzi email non coincidono',
+		PrivacyDisclaimer 					=> 'Lettura e accettazione dell\' <a href="privacy.php">informativa ai sensi del d.lgs. 196/2003</a>',
+		SpamCheck 							=> 'Esegui la seguente operazione',
+		SpamCheckHelp 						=> 'Inserisci il risultato dell\'operazione indicata',
+		MandatoryFields 					=> 'campi obbligatori',
+		
+		// IMMAGINI
+		LogoTitle							=> 'Pagina iniziale Assistenza Rabi Multiservices',
+		LogoAlt								=> 'Logo Rabi Multiservices',
+		
+		// FOOTER 
+		FooterCopyright						=> 'Copyright &copy; 2010 - Rabims.com  - Tutti i diritti riservati - P. IVA',
+		AssistenceHome						=> 'Home Assistenza',
+		Contacts							=> 'Contatti',
+		
+		// ERRORI
+		
+		Err1								=> 'Credenziali di accesso errate...',
+		Err2								=> 'Indirizzo email non valido',
+		Err3								=> 'Sono consentiti solo numeri',
+		Err4								=> 'Compilare i campi...',
+		Err5								=> 'Per poter accedere al servizio &egrave; necessario essere autenticati...',
+		Err6								=> 'Utente non abilitato per questa tipologia di servizio...',
+		Err7								=> 'Per poter procedere al recupero password &egrave; necessario NON essere autenticati',
+		Err8								=> '',
+		Err9								=> '',
+		Err10								=> '',
+	);
+?>
